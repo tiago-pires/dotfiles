2c5180fd5e07aa23448dbab30c3ccc1ab4812292
Tiago Pires <tiago.pires@me.com>
2021-06-02 16:32:35 +1000

    common layout

--- qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/tiago-pires/keymap.c
+++ qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/tiago-pires/keymap.c
@@ -15,23 +15,23 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   
   [_QWERTY] = LAYOUT_5x6(
-     KC_BSPC , KC_1  , KC_2  , KC_3, KC_4  , KC_5  ,                                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EQL,
+     KC_ESC , KC_1  , KC_2  , KC_3, KC_4  , KC_5  ,                                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EQL,
      KC_GRV ,KC_QUOT,KC_COMM, KC_DOT, KC_P  , KC_Y  ,                                   KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_EQL,
-     MT(MOD_LCTL,KC_MINUS),LT(RAISE,KC_A) ,LT(LOWER,KC_O) ,KC_E, KC_U, KC_I,     KC_D , KC_H  , LT(RAISE,KC_T)  , KC_N , KC_S , MT(MOD_RCTL,KC_SLASH), 
-     MT(MOD_LALT,KC_TAB),LT(RAISE,KC_SCLN), KC_Q  , KC_J  , KC_K  , KC_X  ,                    KC_B  , KC_M  , KC_W  , KC_V ,  KC_Z  , MT(MOD_RALT,KC_BSLASH),
-              KC_LBRC, KC_RBRC,                                                               KC_LPRN,KC_RPRN,
-              LT(LOWER,KC_TAB), MT(MOD_LSFT, KC_ESC),                          MT(MOD_RSFT, KC_SPC), LT(RAISE,KC_ENT),
-                 MT(MOD_LSFT, KC_BSPC), MT(MOD_LGUI, KC_EXLM),              MT(MOD_RGUI, KC_EXLM), MT(MOD_RSFT, KC_ENT), 
-                    KC_SPC, LT(DESIGN,KC_ENT),                                           MT(MOD_RALT, KC_ENT), MT(MOD_LSFT, KC_ESC)
+     MT(MOD_LALT,KC_MINUS),KC_A ,LT(DESIGN,KC_O) ,KC_E, KC_U, KC_I,     KC_D , KC_H  , KC_T  , KC_N , KC_S , MT(MOD_RALT,KC_SLASH), 
+     MT(MOD_LSFT,KC_TAB),LT(RAISE,KC_SCLN), KC_Q  , KC_J  , KC_K  , KC_X  ,                    KC_B  , KC_M  , KC_W  , KC_V ,  KC_Z  , MT(MOD_RSFT,KC_BSLASH),
+            KC_LBRC, KC_RBRC,                                                                      KC_LPRN,KC_RPRN,
+           MT(MOD_LCTL,KC_TAB), MT(MOD_LSFT, KC_ESC),                                         MT(MOD_RSFT, KC_SPC), MT(MOD_RCTL,KC_ENT),
+              MT(MOD_LSFT, KC_BSPC), MT(MOD_LGUI, KC_DEL),              MT(MOD_RGUI, KC_BSPC), MT(MOD_LSFT, _______), 
+               KC_SPC, LT(LOWER,KC_ENT),                                RALT(KC_BSPC), MT(MOD_RGUI,_______)
   ),
   
   [_LOWER] = LAYOUT_5x6(
-     _______ , _______  , KC_VOLD  , KC_VOLU  , _______  , _______  ,      _______,_______,KC_LBRC,KC_RBRC,_______,_______,
-     _______, _______  ,  KC_7 , KC_8 , KC_9, LCMD(KC_C) ,                    LCTL(KC_6) , _______ , KC_LPRN, KC_RPRN, _______,_______,
-     _______ , _______, KC_4 , KC_5 , KC_6 , LCMD(KC_V) ,                      _______, KC_BSPC , KC_LCBR, KC_RCBR ,KC_DEL,_______,
-     _______  , _______ , KC_1 , KC_2 , KC_3 , LCMD(KC_X) ,                   _______, _______ , RCTL(KC_W) , _______ ,_______ ,_______,
-            _______ , KC_0,                                            _______, _______,
-                                    _______ , _______,            _______,_______,
+     _______ , _______  , KC_LBRC  , KC_RBRC  , _______  , _______  ,      _______,_______,KC_LBRC,KC_RBRC,_______,_______,
+     KC_0, KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                    KC_CIRC , KC_AMPR , KC_ASTR, KC_LPRN,KC_RPRN,_______,
+     KC_TILD , KC_1, KC_2 , KC_3 , KC_4 , KC_5 ,                           KC_6, KC_7 , KC_8, KC_9 ,KC_0,RCTL(KC_S),
+     _______  , _______ , KC_LCBR , KC_RCBR , _______ , _______ ,     _______, LCTL(KC_6) ,RGUI(KC_BSPC), KC_RBRC ,_______ ,_______,
+                        _______ , KC_0,                                            _______, _______,
+                                    _______ , MT(MOD_LGUI,_______ ),                 KC_ESC,RCTL(KC_ENT),
                                              _______,_______,            _______,_______,
                                              _______,_______,            _______,RESET 
 
@@ -48,13 +48,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______,_______,            _______,RESET
   ),
   [_DESIGN] = LAYOUT_5x6(
-    RESET , _______ , KC_VOLD, KC_VOLU , _______ , _______ ,             LCTL(KC_6)  , _______ , _______ , _______ ,_______ ,_______ ,
-       KC_0,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                             KC_CIRC,KC_AMPR,KC_8,KC_9,KC_RIGHT, KC_BSPC,
-       KC_EXLM,KC_CIRC,LCMD(KC_C),LCMD(KC_V),KC_3,LCMD(KC_LEFT),     KC_BSPC,KC_LEFT,LCTL(KC_S),_______,_______,_______,
-        _______,_______,_______,KC_DOWN,KC_UP,_______,                        RALT(KC_LEFT),LCTL(KC_6),RALT(KC_BSPC),_______,_______,_______,
-                                      LCTL(KC_D) , LCTL(KC_U),            _______,_______,
-                                               _______,KC_0,            KC_0,LCTL(KC_ENT),
-                                               _______,_______,            _______,_______,
+    _______ , _______ , KC_VOLD, KC_VOLU , _______ , _______ ,             LCTL(KC_6)  , _______ , _______ , _______ ,_______ ,_______ ,
+       _______,_______,KC_7,KC_8,KC_9,LCMD(KC_C),                             RCMD(KC_RIGHT),RALT(KC_LEFT),KC_UP,RALT(KC_RIGHT),KC_RIGHT, KC_BSPC,
+       _______,_______,KC_4,KC_5,KC_6,LCMD(KC_V),                             KC_BSPC,KC_LEFT,KC_DOWN,KC_RIGHT,KC_DEL,_______,
+        _______,_______,KC_1,KC_2,KC_3,LCMD(KC_X),                         LCMD(KC_LEFT),RCTL(KC_6),RALT(KC_BSPC),_______,_______,_______,
+                                _______ ,KC_0 ,                      _______,_______,
+                                               _______,_______,            LCTL(KC_D),LCTL(KC_U),
+                                               _______,LSFT(_______),            _______,_______,
                                                _______,_______,            _______,RESET
   ),
 };
